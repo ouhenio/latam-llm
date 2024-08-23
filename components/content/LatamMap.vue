c02579cf186c28f8437f002c3a1055f5fea469a6
oriphel <oriphel@users.noreply.github.com>
2024-08-23 20:59:59 -0300

    content updates (#7)
    * map_update
    
    new countries
    
    * Update_index
    
    Spain was added.
    
    * Fix
    
    The CENIA link has been fixed
    
    * Update
    
    Spain was added
    
    * Update
    
    Description was added
    
    * update
    
    new logo
    
    * update
    
    new photo
    
    * Update
    
    update nuxt

--- components/content/LatamMap.vue
+++ components/content/LatamMap.vue
@@ -18,7 +18,52 @@ const countries = [
     color: "#FF0000"
   },
 
+  {
+    name: "México",
+    coordinates: [-102.5528, 23.6345],
+    color: "#FF0000"
+  },
+
+  {
+    name: "Uruguay",
+    coordinates: [-55.7650, -32.5228],
+    color: "#FF0000"
+  },
+
+  {
+    name: "Argentina",
+    coordinates: [-63.6167, -38.4161],
+    color: "#FF0000"
+  },
+
+  {
+    name: "Colombia",
+    coordinates: [-74.2973, 4.5709],
+    color: "#FF0000"
+  },
+
+  {
+    name: "Ecuador",
+    coordinates: [-78.1834, -1.8312],
+    color: "#FF0000"
+  },
+
+  {
+    name: "Perú",
+    coordinates: [-75.0152, -9.1900],
+    color: "#FF0000"
+  },
+
+  {
+    name: "España",
+    coordinates: [-3.7038, 40.4637],
+    color: "#FF0000"
+  },
+
+
+
+
 ];
-const center = [-77.341225,-4.723156];
-const zoom = 1.5;
+const center = [-60.0, -5.0];
+const zoom = 1;
 </script>
